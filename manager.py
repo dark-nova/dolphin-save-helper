ca576da9a9b85916f8c6e39d22589fa48c3704c3
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-07-18 19:03:40 -0700

    Add type hints and docstrings

--- manager.py
+++ manager.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser(
     )
 parser.add_argument(
     'sub_dir',
-    help='The subdirectory containing .gci files, excluding your save dir root',
+    help='The sub directory containing .gci files, excluding your save dir root',
     )
 parser.add_argument(
     '--slot', '-s',
@@ -30,7 +30,20 @@ GCI_NUMBERS = re.compile(r'(_[0-9]{2})?\.gci', re.I)
 GCI_GLOB = '*.gci'
 
 
-def convert_check_path(directory):
+def convert_check_path(directory: str):
+    """Expands tilde if present (convert). Checks the resulting path
+    for existence.
+
+    Args:
+        directory (str): the `save_dir` arg; the user's directory to use
+
+    Returns:
+        str: the directory, full path
+
+    Raises:
+        Exception: if the directory doesn't exist or isn't a directory
+
+    """
     directory = Path(directory).expanduser()
     if directory.exists() and directory.is_dir():
         return directory
@@ -38,7 +51,22 @@ def convert_check_path(directory):
         raise Exception(f'{directory} doesn\'t exist or isn\'t a directory')
 
 
-def check_file_conflicts(sub_dir, base_dir, card_slot):
+def check_file_conflicts(sub_dir: str, base_dir: str, card_slot: str):
+    """Checks for file conflicts.
+
+    Args:
+        sub_dir (str): the sub dir containing your saves elsewhere
+        base_dir (str): the base dir created and used by Dolphin Emulator
+        card_slot (str): either 'Card A' or 'Card B'
+
+    Returns:
+        bool: True, if files exist in `sub_dir` but not in
+        `base_dir` `card_slot`
+
+    Raises:
+        Exception: if unsuccessful
+
+    """
     non_empty = False
     failure = []
     card_dir = base_dir / card_slot
