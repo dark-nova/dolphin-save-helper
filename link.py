e20cb82bb8db5f6043da7cbbb41b82e94011e290
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2019-08-11 11:23:24 -0700

    Add single file mode for linking

--- link.py
+++ link.py
@@ -3,13 +3,13 @@ from pathlib import Path
 import backup
 
 
-def link_files(sub_dir: str, base_dir: str, card_slot: str, region: str,
-    max_backup: int = 1):
-    """Link files from `sub_dir` into `base_dir` / `card_slot`.
+def link_file(sub_dir: Path, base_dir: Path, card_slot: str, region: str,
+    file: Path, max_backup: int = 1):
+    """Link one file from `sub_dir` into `base_dir` / `card_slot`.
     If `card_slot` doesn't exist, create it.
 
     Due to requiring `sub_dir` and `base_dir` for processing,
-    `link_files` cannot be used in batch.
+    `link_file` cannot be used in batch.
 
     Will automatically call `backup.backup()` per file.
 
@@ -18,11 +18,15 @@ def link_files(sub_dir: str, base_dir: str, card_slot: str, region: str,
         base_dir (str): the base dir created and used by Dolphin Emulator
         card_slot (str): either 'Card A' or 'Card B'
         region (str): 'EUR', 'JAP', 'USA'
+        file (Path): the file
         max_backup (int, optional): maximum circular backup count;
             defaults to 1; should always be >= 1
 
     Returns:
-        bool: True
+        bool: True if successful
+
+    Raises:
+        Exception: if save file doesn't exist or is in the wrong place
 
     """
     card_dir = base_dir / 'GC' / region / card_slot
@@ -30,11 +34,50 @@ def link_files(sub_dir: str, base_dir: str, card_slot: str, region: str,
         print(f'{card_dir.name} doesn\'t exist! Creating...')
         card_dir.mkdir(parents=True)
 
+    if file.resolve().parent != sub_dir:
+        raise Exception(f'{file} isn\'t in {sub_dir}')
+    if file.exists():
+        save_file = file
+    else:
+        save_file = sub_dir / file
+        if not save_file.exists():
+            raise Exception(f"""
+                {save_file} doesn\'t exist.
+                Please check the filename and/or location.
+                """
+                )
+
+    linked = card_dir / file.name
+    linked.symlink_to(save_file)
+    print(f'Linked {save_file.name}!')
+    backup.backup(save_file, max_backup=max_backup)
+
+    return True
+
+
+def link_files(sub_dir: Path, base_dir: Path, card_slot: str, region: str,
+    max_backup: int = 1):
+    """Link files from `sub_dir` into `base_dir` / `card_slot`.
+
+    Calls `link_file` - check docstring for more information.
+
+    Args:
+        sub_dir (Path): the sub dir containing your saves elsewhere
+        base_dir (Path): the base dir created and used by Dolphin Emulator
+        card_slot (str): either 'Card A' or 'Card B'
+        region (str): 'EUR', 'JAP', 'USA'
+        max_backup (int, optional): maximum circular backup count;
+            defaults to 1; should always be >= 1
+
+    Returns:
+        bool: True
+
+    Raises:
+        Exception: see `link_file`
+
+    """
     for file in sub_dir.glob(GCI_GLOB):
-        current = card_dir / file.name
-        current.symlink_to(file)
-        print(f'Linked {file.name}!')
-        backup.backup(file, max_backup)
+        link_file(sub_dir, base_dir, card_slot, region, file, max_backup)
 
     return True
 
